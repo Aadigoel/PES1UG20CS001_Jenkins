d77790a56af725b48c8e8f3d7ea5b166ba9bc8e9
Aadigoel <aadigoel114@gmail.com>
2023-02-14 14:22:55 +0530

    Changed hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "This changes should be reflected in the Jenkins console\n";
   return 0;
 }
